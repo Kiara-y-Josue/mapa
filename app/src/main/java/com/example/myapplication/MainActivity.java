c6c0fcb972edd98dfb515067e6477515d48397a3
Kiara y Josue <kintriagop@uteq.edu.ec>
2024-07-08 11:52:58 -0500

    third commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mapa.getUiSettings().setZoomControlsEnabled(true);
+        mapa.getUiSettings().setMyLocationButtonEnabled(true);
+        mapa.getUiSettings().setMapToolbarEnabled(true);
+
+        CameraUpdate camUpd1 = CameraUpdateFactory
+                .newLatLngZoom(new LatLng(-1.024881800416699, -79.46654681719528), 14);
+        mapa.moveCamera(camUpd1);
+
+        //-1.012561697361079, -79.46946379935433
+        LatLng punto = new LatLng(-1.012561697361079, -79.46946379935433);
+        mapa.addMarker(new MarkerOptions().position(punto)
+                .title("UTEQ"));
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(punto)
+                .radius(200) //En Metros
+                .strokeColor(Color.RED)
+                .fillColor(Color.argb(50, 150, 50, 50));
+
+        Circle circulo = mapa.addCircle(circleOptions);
+
     }
 }
